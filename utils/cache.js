16437940c982a97d3ba28ce5b2415e1bb2180b90
593670711@qq.com <593670711@qq.com>
2020-08-08 02:05:12 +0800

    v1.0.6

--- utils/cache.js
+++ utils/cache.js
@@ -6,7 +6,7 @@ var filePath = path.join(__dirname, './cache.json');
 
 var CACHE = {
     'DEBUG': false, // 开关调试日志记录
-    'refreshShopAdvert': false, // 自动点击商店广告刷新免费商品，领取奖励
+    'refreshShopAdvert': true, // 自动点击商店广告刷新免费商品，领取奖励
     'version': {}, // 版本相关信息
     'mailList': [], // 邮箱数据
     'gonggaoBoard': {}, // 公告数据
